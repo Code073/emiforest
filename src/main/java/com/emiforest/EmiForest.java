eddbac4b6b82b51a52909ab4381ac5d93a1532ca
Pwetty_Pwincess <juancho12g123@gmail.com>
2026-06-30 14:57:19 -0500

    Initial Working Simple Model

--- src/main/java/com/emiforest/EmiForest.java
+++ src/main/java/com/emiforest/EmiForest.java
@@ -1,4 +1,12 @@
 package com.emiforest;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EmiForest.MOD_ID)
 public class EmiForest {
+    public static final String MOD_ID = "emiforest";
+
+    public EmiForest() {
+        System.out.println("EMI Forest initialized!");
+    }
 }
